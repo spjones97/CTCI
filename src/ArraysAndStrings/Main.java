8b8684b229105b5928b5d889a054ae520ea42326
Spencer <sjones97@email.unc.edu>
2021-01-12 22:17:47 -0500

    fix testing

--- src/ArraysAndStrings/Main.java
+++ src/ArraysAndStrings/Main.java
@@ -7,8 +7,8 @@ public class Main {
     public static void main(String[] args) {
         String unique = "abcdefghijklmnopqrstuvwxyz";
         String notUnique = "abcdefghijklmnopqrstuvwxyzz";
-        System.out.println(isUnique(unique));
-        System.out.println(isUnique(notUnique));
+        System.out.println(isUnique(unique) ? "correct" : "error");
+        System.out.println(isUnique(notUnique) ? "error" : "correct");
     }
 
     /*
